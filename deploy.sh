3cd9364d097d4b57d0c03e50cb2d45cd8cf5f63b
remotionis <remotion371@gmail.com>
2026-06-13 04:45:35 +0000

    deploy code draft added

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+echo "Infra auto setup started"
+
+mkdir -p ./dags ./logs ./plugins
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+
+echo "Airflow init started"
+docker compose up airflow-init
+
+echo "docker compose up & setup waiting"
+docker compose up -d
+
+echo "spark consumer starting"
+docker exec -d spark-marter \
+	/opt/spark/bin/spark-submit \
+	--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,io.delta:delta-spark_2.12:3.1.0,com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.16 \
+	--conf spark.hadoop.fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem \
+	--conf spark.hadoop.fs.AbstractFileSystem.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS \
+	/opt/spark/scripts/consumer.py
+
+echo "Infra setup & consumer auto-start completed"
